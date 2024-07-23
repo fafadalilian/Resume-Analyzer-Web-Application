baeac8e9137dae4a072b65fd66e1dcd3c9dd3284
Fatemeh Dalilian <54078617+fafadalilian@users.noreply.github.com>
2024-07-23 10:32:11 -0500

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -16,17 +16,6 @@ google_api_key = st.secrets["GOOGLE_API_KEY"]
 
 
 
-
-# def get_resume():
-#     resume_text = st.text_area(label="resume Input", label_visibility='collapsed', placeholder="Your resume...", key="resume_input")
-#     return resume_text
-
-# def get_job():
-#     job_text = st.text_area(label="job Input", label_visibility='collapsed', placeholder="Your job desciption...", key="job_input")
-#     return job_text
-
-
-
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro",
                              temperature=0.3)
 
@@ -48,18 +37,18 @@ Years of industry experience:
 """
 
 resume_prompt = """
-Extract the main keywords from the following resume and format them in a clear, structured manner :
+List all the keywords implied from the following resume and format them in a structured manner :
 
 Resume:
 {text}
 
 ## Keywords:
 
-- List each keyword
+- List each keyword (where this keyword is extracted and implied from)
  """
 
 match_prompt = """
-Given the list of qualifications and keywords extracted from the job description, and the qualifications listed on the resume, provide the following (do not make up things or recommendations if the resume is good):
+Given the list of qualifications and keywords extracted from the job description, and the qualifications as well as implied qualification listed on the resume, provide the following (do not make up things or recommendations if the resume is good):
 
 1. A list of qualifications from the resume that match the job description keywords and qualifications.
 2. A list of qualifications from the resume that do not match the job description keywords and qualifications.
@@ -74,14 +63,13 @@ Given the list of qualifications and keywords extracted from the job description
 
 ## Output:
 ### Matching Qualifications:
-- List each matching qualification
+- List each matching qualification (where this is extracted or implied from)
+
+### Qualifications you might wanna add:
+- List each qualification needed in the job description not mentioned in the resume. (where this keyword is extracted or implied from)
 
 ### Non-Matching Qualifications:
-- List each non-matching qualification
-
-
-### Qualifications to be added:
-- List each qualification needed in the job description not mentioned in the resume
+- List each non-matching qualification (if it is beneficial to have on the resume)
 
 ### Recommendations:
 - List each recommendation
@@ -113,75 +101,7 @@ def match_analysis(job_text, resume_text):
   return match_result
 
 
-# Create a placeholder for the results at the top of the page
-#result_placeholder = st.empty()
 
-# job_text = get_job()
-# if len(job_input.split(" ")) > 700:
-#     st.write("Please enter a shorter job description. The maximum length is 700 words.")
-#     st.stop()
-# resume_text 
-
-
-# if len(email_input.split(" ")) > 700:
-#     st.write("Please enter a shorter email. The maximum length is 700 words.")
-#     st.stop()
-
-# def parse_response(response):
-#     sections = {
-#         "matching_qualifications": [],
-#         "non_matching_qualifications": [],
-#         "qualifications_to_be_added": [],
-#         "recommendations": []
-#     }
-
-#     current_section = None
-#     for line in response.split('\n'):
-#         line = line.strip()
-#         if line.startswith("###"):
-#             if "Matching Qualifications" in line:
-#                 current_section = "matching_qualifications"
-#             elif "Non-Matching Qualifications" in line:
-#                 current_section = "non_matching_qualifications"
-#             elif "Qualifications to be added" in line:
-#                 current_section = "qualifications_to_be_added"
-#             elif "Recommendations" in line:
-#                 current_section = "recommendations"
-#         elif line.startswith("- ") and current_section:
-#             item = line[2:]  # Remove the leading "- "
-#             sections[current_section].append(item)
-    
-#     # Number the items in each section
-#     for section in sections:
-#         sections[section] = [f"{i+1}. {item}" for i, item in enumerate(sections[section])]
-    
-#     return sections
-
-
-
-# # Function to display the analysis results
-# def display_results(results):
-#     col1, col2, col3, col4 = st.columns(4)
-
-#     with col1:
-#         st.markdown("### Matching Qualifications:")
-#         for item in results["matching_qualifications"]:
-#             st.write(f"- {item}")
-
-#     with col2:
-#         st.markdown("### Non-Matching Qualifications:")
-#         for item in results["non_matching_qualifications"]:
-#             st.write(f"- {item}")
-
-#     with col3:
-#         st.markdown("### Qualifications to be Added:")
-#         for item in results["qualifications_to_be_added"]:
-#             st.write(f"- {item}")
-
-#     with col4:
-#         st.markdown("### Recommendations:")
-#         for item in results["recommendations"]:
-#             st.write(f"- {item}")
 
 # Main function to run the Streamlit app
 def main():
@@ -199,7 +119,7 @@ def main():
         with st.spinner("Analyzing..."):
             results = match_analysis(job_input, resume_input)
             st.write(results)
-            #display_results(results)
+
 
 if __name__ == "__main__":
     main()
